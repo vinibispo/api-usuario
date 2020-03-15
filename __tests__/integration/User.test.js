56e3e432b5c19c9593ea86235f00671db197999e
vinibispo <vini.bispo015@gmail.com>
2020-03-15 14:23:52 -0300

    Teste DELETE funcionando

--- __tests__/integration/User.test.js
+++ __tests__/integration/User.test.js
@@ -4,6 +4,10 @@ const User = require('../../src/models/User')
 const request = supertest(app)
 
 describe('Controller de Usuário', ()=>{
+    beforeAll(async()=>{
+        await User.deleteMany({})
+        await User.create({name: 'Teste', email: 'teste@teste', password: 'pass'})
+    })
     it('Deve retornar 302 quando vai para a rota /', async()=>{
         const response = await request.get('/')
         expect(response.status).toBe(302)
@@ -18,12 +22,18 @@ describe('Controller de Usuário', ()=>{
         expect(response.body[0].name).toBe('Teste')
     })
     it('Deve retornar 201, quando o usuário for para a rota POST /users/add', async()=>{
-        const response  = await request.post('/users/add').send({name: 'Teste', email: 'teste@teste', senha: 'senha'})
+        const response  = await request.post('/users/add').send({name: 'Teste', email: 'teste@teste', password: 'senha'})
         expect(response.status).toBe(201)
     })
     it('Deve retornar 200 quando o usuário utilizar a rota 200 PUT /:id', async()=>{
         const users = await User.find({})
         const user = users[0]
-        const response = await request.put(`/users/${user._id}`)
+        const response = await request.put(`/users/${user._id}`).send({name: 'Teste 2', email: 'teste@teste.com', password:'senha123'})
+        expect(response.status).toBe(200)
+    })
+    it('Deve retornar 204 quando um usuário for para a rota DELETE /users/:id', async()=>{
+        const users =  await User.find({})
+        const user = users[0]
+        const response = await request.delete(`/users/${user._id}`)
     })
 })
