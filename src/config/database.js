de2eca1e47ef33e07b95dbe01ef839c580567d15
vinibispo <vini.bispo015@gmail.com>
2020-03-15 13:06:48 -0300

    Configurando arquivo de conexão

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+if(process.env.NODE_ENV == "test"){
+    require('dotenv').config({path: '.env.test'})
+}else{
+    require('dotenv').config({path: '.env'})
+}
+const myDb = {
+    connect: mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true}),
+    once: mongoose.connection.once('open', ()=>{
+             console.log('Connected to MongoDB')
+         }),
+    error: db.on('error', (err)=>{
+           console.log(err)
+       })
+}
+module.exports = myDb
