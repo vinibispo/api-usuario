3f30975ae403434e0947eeb77650406a7343b7fb
vinibispo <vini.bispo015@gmail.com>
2020-03-15 13:56:29 -0300

    Era para o teste falhar, mas agora é pra funcionar

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors())
 app.use('/users', routes)
 
+app.get('/', (req, res)=>{
+    res.redirect('/users')
+})
 database.connect
 database.once
 database.error
