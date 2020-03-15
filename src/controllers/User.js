c26991b437542a73be0dcb86f35d0ff7c26fa3fe
vinibispo <vini.bispo015@gmail.com>
2020-03-15 12:53:54 -0300

    Controller quase completo

---
+++ src/controllers/User.js
@@ -0,0 +1,37 @@
+const UserController =  {
+    findAll: async(req, res)=>{
+        try {
+            const users = await User.find({})
+            res.status(200).send(users)
+        } catch (error) {
+            res.status(401).send(error)
+        }
+    },
+    add: async(req, res)=>{
+        try {
+            const user = await User.create({name: req.body.name, email: req.body.email, password: req.body.password})
+            res.status(201).send(user)
+        } catch (error) {
+            res.status(201).send(error)
+        }
+    },
+    update: async(req, res)=>{
+        const id = req.params.id
+        const {name, email, password} = req.body
+        try {
+            const user = await User.findByIdAndUpdate(id)
+            res.status(200).send(user)
+        } catch (error) {
+            res.status(401).send(error)
+        }
+    },
+    exclude: async(req, res) =>{
+        const id = req.params.id
+        try {
+            await User.findByIdAndDelete(id)
+            res.status(204).send()
+        } catch (error) {
+            res.status(401).send(error)
+        }
+    }
+}
