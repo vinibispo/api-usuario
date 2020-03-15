d39cb8a113988df6f320c482679b0a9c3e3efe92
vinibispo <vini.bispo015@gmail.com>
2020-03-15 13:09:36 -0300

    Faltava configurar no mongodb

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
-
+const database = require('./config/database')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 app.use('/users', routes)
+
+database.connect
+database.once
+database.error
 
 module.exports = app
