96de328ca1e87a8b1c6a7421d0df78b525d905cc
vinibispo <vini.bispo015@gmail.com>
2020-03-15 12:58:21 -0300

    Criando  model Usuário

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const {Schema, Model} = require('mongoose')
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: String
+    }
+})
+module.exports = new Model(UserSchema, 'User')
