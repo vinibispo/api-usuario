48fa3fc8b30a5c467276af47acc8abaee824e7b7
vinibispo <vini.bispo015@gmail.com>
2020-03-17 12:05:52 -0300

    Modificando utils do redis

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,7 +1,28 @@
 const  redisClient = require('../config//redis')
 
-const GET = (key) =>redisClient.get(key)
-const SET = (key, value)=> redis.set(key, value, 'EX', 60)
+const GET = (key) =>{
+    return new Promise((resolve, reject)=>{
+        redisClient.get(key, (err, value)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(value)
+            }
+        })
+    })
+}
+const SET = (key, value) =>{
+    return new Promise((resolve, reject)=>{
+        redisClient.set(key, value, 'EX', 60, (err)=>{
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(true)
+            }
+        })
+    })
+}
 const redis = {
     get: GET,
     set: SET,
