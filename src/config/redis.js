099e483b7d7d143012ceb0c0249506864b7c11c6
vinibispo <vini.bispo015@gmail.com>
2020-03-17 11:16:57 -0300

    Testando conexão do redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,5 +1,8 @@
 const redis = require('redis')
 
 const redisClient = redis.createClient(process.env.REDIS_URL)
+redisClient.on('error', (err)=>{
+    console.error(err)
+})
 
 module.exports = redisClient
